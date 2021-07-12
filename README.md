693952a1375057e832c724a9b7a179483d9d0967
Igor Bălănici <igor.balanici@gmail.com>
2021-07-13 00:04:46 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+from here:
+
+https://spring.io/guides/gs/spring-boot-kubernetes/
---
+++ src/main/java/com/balanici/jarink8s/JarInK8sApplication.java
@@ -0,0 +1,13 @@
+package com.balanici.jarink8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarInK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarInK8sApplication.class, args);
+	}
+
+}
